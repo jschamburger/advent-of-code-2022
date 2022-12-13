95fb2c61946005aa60f67a15349c7925c8a4998f
Johannes Schamburger <johannes.schamburger@inovex.de>
2022-12-13 17:42:40 +0100

    Add implementation for day 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,15 +3,26 @@ fun main() {
         return input.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+
+    val elves = mutableListOf<MutableList<String>>()
+
+    input.forEach {
+        if (elves.isEmpty()) {
+            elves.add(mutableListOf(it))
+        } else if (it.isEmpty()) {
+            elves.add(mutableListOf())
+        } else {
+            elves.last().add(it)
+        }
+    }
+
+    elves.println()
+    val elvesSortedByCalories = elves.map { l -> l.sumOf { it.toInt() } }.sortedDescending()
+    // first part
+    elvesSortedByCalories.first().println()
+    // second part
+    elvesSortedByCalories.subList(0, 3).sum().println()
+
 }
