d31ebcb9cedd3bde347ad5ae6ab23745ba51185a
Johannes Schamburger <johannes.schamburger@inovex.de>
2022-12-14 14:20:13 +0100

    Improve day 2 solution.

--- src/Day02.kt
+++ src/Day02.kt
@@ -8,7 +8,9 @@ fun main() {
             val split = it.split(" ")
             val opponent = split[0]
             val player = split[1]
-            calculateScore(opponent, player)
+            val opponentShape = SealedShape.fromString(opponent)
+            val playerShape = SealedShape.fromString(player)
+            calculateScore(opponentShape, playerShape)
         } else 0
     }
     sum.println()
@@ -25,79 +27,71 @@ fun main() {
     sum2.println()
 }
 
-fun calculateScore(opponent: String, player: String): Int {
-    val opponentShape = Shape.fromString(opponent)
-    return when (Shape.fromString(player)) {
-        Shape.ROCK -> {
-            1 + when (opponentShape) {
-                Shape.ROCK -> 3
-                Shape.PAPER -> 0
-                Shape.SCISSORS -> 6
-            }
-        }
-
-        Shape.PAPER -> {
-            2 + when (opponentShape) {
-                Shape.ROCK -> 6
-                Shape.PAPER -> 3
-                Shape.SCISSORS -> 0
-            }
-        }
-
-        Shape.SCISSORS -> {
-            3 + when (opponentShape) {
-                Shape.ROCK -> 0
-                Shape.PAPER -> 6
-                Shape.SCISSORS -> 3
-            }
-        }
-    }
+fun calculateScore(opponent: SealedShape, player: SealedShape): Int {
+    return player.calculateScore(opponent) + player.value
 }
 
 fun calculateScoreWithOutcome(opponent: String, outcomeString: String): Int {
     val outcome = Outcome.fromString(outcomeString)
-    val playerShape = when (Shape.fromString(opponent)) {
-        Shape.ROCK -> {
-            when (outcome) {
-                Outcome.LOSE -> Shape.SCISSORS
-                Outcome.DRAW -> Shape.ROCK
-                Outcome.WIN -> Shape.PAPER
-            }
+    val opponentShape = SealedShape.fromString(opponent)
+    return calculateScore(opponentShape, opponentShape.getShapeForOutcome(outcome))
+}
+sealed class SealedShape(val value: Int) {
+
+    abstract val mapOutcome: Map<Outcome, SealedShape>
+
+    fun getShapeForOutcome(outcome: Outcome) = mapOutcome[outcome.reverse()] ?: throw IllegalArgumentException()
+
+    fun calculateScore(opponentShape: SealedShape): Int {
+        val reverse = mapOutcome.reverse()
+        val outcome = reverse[opponentShape] ?: throw IllegalArgumentException()
+        return when (outcome) {
+            Outcome.LOSE -> 0
+            Outcome.DRAW -> 3
+            Outcome.WIN -> 6
         }
-        Shape.PAPER -> {
-            when (outcome) {
-                Outcome.LOSE -> Shape.ROCK
-                Outcome.DRAW -> Shape.PAPER
-                Outcome.WIN -> Shape.SCISSORS
-            }
-        }
-        Shape.SCISSORS -> {
-            when (outcome) {
-                Outcome.LOSE -> Shape.PAPER
-                Outcome.DRAW -> Shape.SCISSORS
-                Outcome.WIN -> Shape.ROCK
+    }
+    companion object {
+        fun fromString(string: String): SealedShape {
+            return when (string) {
+                "A", "X" -> Rock
+                "B", "Y" -> Paper
+                "C", "Z" -> Scissors
+                else -> throw IllegalArgumentException("unknown shape")
             }
         }
     }
-    return calculateScore(opponent, playerShape.playerStringValue)
-}
+    object Rock : SealedShape(1) {
+        override val mapOutcome = mapOf(Outcome.WIN to Scissors, Outcome.LOSE to Paper, Outcome.DRAW to Rock)
+    }
 
-enum class Shape(private val opponentStringValue: String, val playerStringValue: String) {
-    ROCK("A", "X"), PAPER("B", "Y"), SCISSORS("C", "Z");
+    object Paper : SealedShape(2) {
+        override val mapOutcome = mapOf(Outcome.WIN to Rock, Outcome.LOSE to Scissors, Outcome.DRAW to Paper)
+    }
 
-    companion object {
-        fun fromString(string: String): Shape {
-            return Shape.values().first { shape -> shape.opponentStringValue == string || shape.playerStringValue == string }
-        }
+    object Scissors : SealedShape(3) {
+        override val mapOutcome = mapOf(Outcome.WIN to Paper, Outcome.LOSE to Rock, Outcome.DRAW to Scissors)
     }
 }
 
 enum class Outcome(private val stringValue: String) {
     LOSE("X"), DRAW("Y"), WIN("Z");
 
+    fun reverse(): Outcome {
+        return when(this) {
+            LOSE -> WIN
+            DRAW -> DRAW
+            WIN -> LOSE
+        }
+    }
+
     companion object {
         fun fromString(string: String): Outcome {
             return Outcome.values().first { it.stringValue == string }
         }
     }
+}
+
+fun <K, V>Map<K, V>.reverse(): Map<V, K> {
+    return this.entries.associate { (k, v) -> v to k }
 }
